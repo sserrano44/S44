6d3fa20c9894c159cc37aa1ae4b9c886a9110e50
Sebastian Serrano <sebastian@ripio.com>
2018-03-23 18:36:22 -0300

    initial

---
+++ contracts/S44.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.11;
+
+import "./MintableToken.sol";
+
+contract S44 is MintableToken {
+    string public constant name = "sserrano44 coin";
+    string public constant symbol = "S44";
+    uint8 public constant decimals = 18;
+    string public version = "1.0";
+}
---
+++ contracts/StandardToken.sol
@@ -0,0 +1,77 @@
+pragma solidity ^0.4.11;
+
+import "./zeppelin/SafeMath.sol";
+
+contract Token {
+    uint256 public totalSupply;
+    function balanceOf(address _owner) constant returns (uint256 balance);
+    function transfer(address _to, uint256 _value) returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+    function approve(address _spender, uint256 _value) returns (bool success);
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+
+/*  ERC 20 token */
+contract StandardToken is Token {
+    using SafeMath for uint256;
+    function transfer(address _to, uint256 _value) returns (bool success) {
+      if (balances[msg.sender] >= _value) {
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+      if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value) {
+        balances[_to] = balances[_to].add(_value);
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender,  uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+    }
+
+    function increaseApproval (address _spender, uint _addedValue) public returns (bool success) {
+      allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+      Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+      return true;
+    }
+
+    function decreaseApproval (address _spender, uint _subtractedValue) public returns (bool success) {
+      uint oldValue = allowed[msg.sender][_spender];
+      if (_subtractedValue > oldValue) {
+        allowed[msg.sender][_spender] = 0;
+      } else {
+        allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+      }
+      Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+      return true;
+    }
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+}
+
